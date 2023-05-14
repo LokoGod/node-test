c54279e344b41981f5a3beecd8f93bc988c247cb
LokoGod <it21079672@my.sliit.lk>
2023-05-14 13:51:40 +0530

    nodemon

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-const {readFile, writeFile} = require('fs').promises;
-// const util = require('util');
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const EventEmitter = require('events');
 
- const start = async() => {
-    try {
-    const first = await readFile('./content/first.txt', 'utf8');
-    const second = await readFile('./content/second.text', 'utf8');
-    await writeFile('./content/result-mind-grenade.txt', `THIS IS AWESOME : ${first} ${second}`, {flag: 'a'});
-    console.log(first, second);
+const customEmitter = new EventEmitter();
 
-} catch (error) {
-    console.log(error);
-    }
- }
+// on   - listen for an event
+// emit - emit an event
 
- start()
- 
-// const getText = (path) => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path,'utf8',(err,data) => {
-//             if(err) {
-//                 reject(err);
-//             }
-//             else {
-//                 resolve(data)
-//             }
-//         })
+customEmitter.on('response', () => {
+    console.log(`data recieved`);
+});
 
-//     })
-// }
-
-
-
-// getText('./content/first.txt')
-//  .then((result) => console.log(result))
-//  .catch((err) => console.log(err))
-
+customEmitter.emit('response');

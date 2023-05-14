ee9c2d0cab00488fafd079704e0435ed9bd66a26
LokoGod <it21079672@my.sliit.lk>
2023-05-14 16:32:31 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    const text = fs.readFileSync("./content/big.txt", 'utf-8')
-    res.end(text)
+    // const text = fs.readFileSync("./content/big.txt", 'utf-8')
+    // res.end(text)
+    const filestream = fs.createReadStreamm(("./content/big.txt", 'utf-8'));
+    filestream.on('open', () => {
+         filestream.pipe();
+    })
+    filestream.on('error', (err) => {
+        res.end(err);
+    })
 }) 
 .listen(5000);
